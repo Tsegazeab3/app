d819b9b282f864613e630d43cea0b4d795fee712
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2025-06-02 11:10:00 +0400

    commiting for checking later

---
+++ student_schedule_app/notifications_and_alarms/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NotificationsAndAlarmsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notifications_and_alarms'
---
+++ student_schedule_app/personalized_planning_and_recommendation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PersonalizedPlanningAndRecommendationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'personalized_planning_and_recommendation'
